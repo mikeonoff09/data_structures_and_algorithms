4512fb9f0d20d357f1d11dd8e8fae40910cb10c8
Miguel Lara <miguel.lara@soytul.com>
2023-07-31 14:05:00 -0500

    initial commit

---
+++ bin/data_structures_and_algorithms.dart
@@ -0,0 +1,5 @@
+import 'package:data_structures_and_algorithms/data_structures_and_algorithms.dart' as data_structures_and_algorithms;
+
+void main(List<String> arguments) {
+  print('Hello world: ${data_structures_and_algorithms.calculate()}!');
+}
